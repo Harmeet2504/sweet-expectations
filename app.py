9177fdf38a77bc525c01bc1812ef9ba53ba6e500
Harmeet Kaur <meet.academia@gmail.com>
2020-06-19 12:10:13 -0400

    initial app testing

---
+++ app.py
@@ -0,0 +1,49 @@
+#importing libraries
+from flask import Flask, jsonify, render_template, request
+from sklearn.ensemble import RandomForestClassifier
+import os
+import numpy as np
+import re
+import pickle
+
+
+
+app = Flask(__name__)
+Best_model = pickle.load(open('model_tree.pkl', 'rb'))
+
+@app.route("/")
+# @app.route("/index")
+def index():
+    """Return the homepage."""
+    return render_template("test-index.html", )
+
+@app.route('/predict')
+def predict():
+    return render_template("monitor.html",status="predict" )
+
+
+@app.route('/resultCR',methods = ['POST'])
+def resultCRNN():
+    if request.method == 'POST': 
+        patient_info = request.form.to_dict()
+        int_features = [int(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        # print(final_features)
+        prediction = Best_model.predict_proba(final_features)
+        output=np.round(prediction[0,1]*100,2)
+        # print(output)
+        if int(output)>=65:
+            prediction='High Risk for Gestational Diabetes'  
+            risk=3      
+        elif int(output)>=45:
+            prediction='Moderate Risk for Gestational Diabetes' 
+            risk=2   
+        else:
+            prediction='You are healthy'
+            risk=1
+        print(prediction)
+        return render_template('monitor.html', prediction_text='{}'.format(prediction), status="results", risk=risk)
+        
+
+if __name__ == '__main__':
+    app.run()
